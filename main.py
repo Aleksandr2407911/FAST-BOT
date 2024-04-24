1d92b0af5ff672aa66f8449cb135933b253ec3e5
VB <vb@MacBook-Pro-VB.local>
2024-04-24 18:36:19 +0300

    4

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ import user_module
 import admin_module
 from user_module import list_of_admins
 
-async def clear_admin_list():
+async def clear_admin_list(list_of_admins):
     while True:
         await asyncio.sleep(600)  # Подождать 10 минут
         list_of_admins.clear()  # Очистить список администраторов
 
 #функция конфигурирования и запуска бота
 async def main():
-    
-    await clear_admin_list()
     
     #регистрируем бота и диспетчер
     bot = Bot(token= '6982700202:AAGziEKEE79FgTdwtUjebPsfUbVcMV_w_p0')
@@ -26,6 +24,7 @@ async def main():
     #пропускаем накопившиемя апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+    await clear_admin_list(list_of_admins)
 
 if __name__ == '__main__':
     print('Запуск прошел успешно')
